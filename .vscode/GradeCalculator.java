8aea42de73f0411ebd5c5423775d4dd0a9016098
Elif Bozkurt <elfbzkrtt07@gmail.com>
2025-03-26 17:59:39 +0300

    course selection screen

--- .vscode/GradeCalculator.java
+++ .vscode/GradeCalculator.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class GradeCalculator extends JFrame {
     private JPanel coursePanel;
     private ArrayList<Course> courses;
+    private Course selected = null;
 
     public GradeCalculator() {
         this.setSize(400, 300); 
@@ -18,26 +19,59 @@ public class GradeCalculator extends JFrame {
         this.setVisible(true);
     }
 
-    public void courseSelectionMenu() {
+    public Course courseSelectionMenu() {
         coursePanel = new JPanel();
-        coursePanel.setLayout(new BoxLayout(coursePanel, BoxLayout.Y_AXIS)); 
-
-        ButtonGroup bg = new ButtonGroup();
-
-        for (Course course : this.courses) {
+        coursePanel.setLayout(new BorderLayout()); 
+        JLabel label = new JLabel("Select a course:", SwingConstants.CENTER);
+        ArrayList<JRadioButton> radioButtons = new ArrayList<>();
+        ButtonGroup group = new ButtonGroup();
+    
+        JButton continueButton = new JButton("Continue");
+        continueButton.setEnabled(false);
+        continueButton.setSize(new Dimension(50,10));
+    
+        for (Course course : courses) {
             JRadioButton rb = new JRadioButton(course.getCourseName());
-            bg.add(rb);
+            radioButtons.add(rb);
+            group.add(rb);
             coursePanel.add(rb);
+    
+            rb.addActionListener(e -> {
+                continueButton.setEnabled(true);
+            });
         }
-
+    
+        continueButton.addActionListener(e -> {
+            for (JRadioButton rb : radioButtons) {
+                if (rb.isSelected()) {
+                    String selectedCourse = rb.getText();
+                    selected = getCourseByName(selectedCourse);
+                    break;
+                }
+            }
+    
+            JOptionPane.showMessageDialog(this, "You selected: " + selected.getCourseName());
+        });
+        
+        coursePanel.add(label, BorderLayout.NORTH);
+        coursePanel.add(continueButton, BorderLayout.SOUTH);
         this.add(coursePanel);
-    }
+        return selected;
+    }    
 
     public void initializeCourses() {
         courses = new ArrayList<>();
         courses.add(new Math102());
     }
 
+    public Course getCourseByName(String name) {
+        for (Course c : courses) {
+            if (c.getCourseName().equals(name)) {
+                return c;
+            }
+        }
+        return null;
+    }
     public static void main(String[] args) {
         new GradeCalculator();
     }
